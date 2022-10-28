ec718269854aa0f0a67d518891bcea7fa9538d72
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-10-28 05:03:49 +0300

    Добавьте файлы проекта.

---
+++ Классы и объекты/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Классы_и_объекты
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person raziyat = new Person();
+            raziyat.name = "Raziyat";
+            raziyat.age = 16;
+            raziyat.Print();
+
+            Console.WriteLine(Person.type);
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public const string type = "Person";
+        public string name;
+        public int age;
+
+        public void Print()
+        {
+            Console.WriteLine($"{type}: {name} - {age}");
+        }
+    }
+}
